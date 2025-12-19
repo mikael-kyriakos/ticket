8ff9cba64f6ef23415d77dfa5c0f97673c647f3e
mikaelkyriakos <mikaelkyriakos3@gmail.com>
2025-12-19 11:04:57 +0000

    learning to add top appBar and bottom navigation bar, seeing how this looks

--- ticketApp/ticket_app/lib/main.dart
+++ ticketApp/ticket_app/lib/main.dart
@@ -11,21 +11,30 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // CAN REMOVE IN FINAL APP - FOR CLEANER DEBUG
+      debugShowCheckedModeBanner: false,
+
       home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Center(
-                child: Text('A random AWESOME idea:'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print('button pressed!');
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          title: Text("BathTicket", style: TextStyle(fontWeight: FontWeight.bold)),
+          centerTitle: true,
+          backgroundColor: Colors.teal[300],
+        ),
+        body: Column(
+          children: [
+            
+          ]
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket_outlined), label: 'Marketplace'),
+            BottomNavigationBarItem(icon: Icon(Icons.merge_type_rounded), label: 'Messages'),
+            BottomNavigationBarItem(icon: Icon(Icons.person_outline_rounded), label: 'Profile'),
+          ],
+          currentIndex: 0,
+          selectedItemColor: Colors.teal,
+          elevation: 0,
         ),
       ),
     );
