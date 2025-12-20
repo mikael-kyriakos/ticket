a9530b101fc7aeec151e27165ad7fc6ae1736fd9
mikaelkyriakos <mikaelkyriakos3@gmail.com>
2025-12-20 12:54:25 +0000

    New recreated app - uses listview builder for showing tickets, has buy / sell pages which aren't yet switchable between. First iteration finished

---
+++ ticketApp/bathticket_app/android/app/src/main/kotlin/com/example/bathticket_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bathticket_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ticketApp/bathticket_app/lib/main.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 44, 87, 151)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'BathTicket'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  var arr = [1,2,3,4,5,6,7,8,9,10];
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              child: Text(
+                "Available Tickets:",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: arr.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 100,
+                          margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                          child: Center(child: Text('Ticket ${arr[index]}')),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            color: Color.fromARGB(255, 236, 236, 236),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        margin: EdgeInsets.fromLTRB(0, 20, 20, 0),
+                        child: Center(child: Text('BUY')),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color: Color.fromARGB(255, 255, 217, 0),
+                        ),
+                      ),
+                    ],
+                  );
+                }
+              ),
+            ),
+          ],
+        )
+        /*
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have:',
+            ),
+            Text(
+              '£$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+        */
+      ),
+      /*
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+      */
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.featured_play_list_outlined),
+            label: 'Buy',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money_rounded),
+            label: 'Sell',
+          ),
+        ],
+        elevation: 0,
+        currentIndex: 0,
+      ),
+    );
+  }
+}
