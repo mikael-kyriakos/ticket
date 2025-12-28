f6d7fb49df05739a5d102e4d8a19503b52a040fa
Mikael Kyriakos <mikaelkyriakos3@gmail.com>
2025-12-28 22:49:42 +0000

    quick code edit

--- ticketApp/bathticket_app/lib/main.dart
+++ ticketApp/bathticket_app/lib/main.dart
@@ -15,7 +15,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,18 +22,6 @@ class MyApp extends StatelessWidget {
       // NOTE - CAN DELETE THE ABOVE PROPERTY ONCE DONE WITH APP
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 44, 87, 151)),
         useMaterial3: true,
         scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255),
@@ -306,7 +293,6 @@ class SellPage extends StatelessWidget {
 
 class TicketsPage extends StatelessWidget {
   TicketsPage({super.key});
-
 
   // Note: would be good to have a character limit for event names, to prevent overflowing (note: this may be a different point depending on device too)
   // Assuming the list taken from the database is ordered correctly by date (ascending for upcoming, descending for past)
